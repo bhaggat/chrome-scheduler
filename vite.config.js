1733b06bf378f474040cb5ab9eb293c8971a13cb
webosmotic-developer <dev.webosmotic@gmail.com>
2024-10-28 14:25:41 +0530

    first render

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        popup: 'index.html',
+        background: 'src/background.js',
+        content: 'src/content.js'
+      },
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]'
+      }
+    }
+  }
+});
